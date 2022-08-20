92ba5f9357c2feb16d6ba27e672c111787ccc2a7
Julia <yagubetslulka@yandex.ru>
2022-08-20 14:19:45 +0300

    Задача 3. Определитель четного числа

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int numberA=Convert.ToInt32(ReadLine());
+if(numberA%2==0)
+{
+    WriteLine("Четное");
+}
+else
+{
+    WriteLine("Нечетное");
+}
