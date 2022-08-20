3c74e956e16929f9806ed4ec6583d62e301cbe82
Julia <yagubetslulka@yandex.ru>
2022-08-20 14:24:29 +0300

    Задача 4. Четные числа от 1 до N

---
+++ 5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int numberN=Convert.ToInt32(ReadLine());
+int numberA=1;
+while(numberA<=numberN)
+{
+    if(numberA%2==0)
+    {
+        WriteLine(numberA);
+    }
+    numberA++;
+}
