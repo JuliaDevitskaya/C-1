44d64a5194f5e9d6f38bbb60af9b8d438f83770b
Julia <yagubetslulka@yandex.ru>
2022-08-23 15:06:28 +0300

    корректировка задачи после проверки

--- 2/Program.cs
+++ 2/Program.cs
@@ -13,10 +13,6 @@ WriteLine("Введите третье число");
 int numberC = Convert.ToInt32(ReadLine());
 int max = numberA;
 
-if (numberA>max)
-{
-    max=numberA;
-}
 if(numberB>max)
 {
     max=numberB;
